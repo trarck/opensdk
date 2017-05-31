d6c47db023a39058a70f34093bf33b497fe7709e
trarck <trarck@gmail.com>
2017-05-31 17:05:12 +0800

    加入显示文件列表。

---
+++ tools/listfile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+list_alldir()
+{
+    for file in $1/*
+    do
+        if [ -f $file ]; then
+            echo "'"${file}"'",
+        fi
+        
+        if [ -d $file ]; then
+            list_alldir $file            
+        fi
+    done     
+}
+
+if [ $# -gt 0 ]; then
+    list_alldir "$1"
+else
+    list_alldir "."
+fi
